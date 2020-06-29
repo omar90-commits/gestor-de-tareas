cc89e6545b013c756cc4912d4231957be2fcb4f3
omar90-commits <omravaja@hotmail.com>
2020-06-29 17:25:10 -0400

    quitando variables no utilisadas

--- src/Modulos/editarTarea.js
+++ src/Modulos/editarTarea.js
@@ -7,11 +7,7 @@ export let id = null;
 
 export const editarTarea = (tarea, btn) => {	
 	let btnEditar = document.querySelector(`#editar-${tarea.id}`);
-	
 	btnEditar.addEventListener('click', () => {
-		let btnEliminarObtener = document.querySelector(`#eliminar-${tarea.id}`),
-			btnEliminarAllObtener = Array.from(document.querySelectorAll('.btn-danger'));		
-
 		disabledBtnEliminar(tarea, true);
 
 		tareaInput.value = tarea.tareaValue;
